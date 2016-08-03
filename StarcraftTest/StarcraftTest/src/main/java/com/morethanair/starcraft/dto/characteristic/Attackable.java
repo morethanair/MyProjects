ad1b0ca5bfba9d209a15e88aabee20483e43f75f
MoreThanAir <MoreThanAir@localhost>
2016-08-03 20:58:17 +0900

    뭣이 중헌디?

--- StarcraftTest/StarcraftTest/src/main/java/com/morethanair/starcraft/dto/characteristic/Attackable.java
+++
@@ -1,9 +0,0 @@
-package com.morethanair.starcraft.dto.characteristic;
-
-import com.morethanair.starcraft.dto.StarcraftObject;
-
-public interface Attackable {
-	public void attack();
-
-	public void attack(StarcraftObject starcraftObject);
-}
